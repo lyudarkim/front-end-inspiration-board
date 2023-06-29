38df0f3f37f0e2d340962e42f9aef3107a957a57
selene castillo <secast@live.com>
2023-06-29 11:41:30 -0700

    Initialize and render CardForm
    Co-authored-by: sophiat8832 <sophiat8832@users.noreply.github.com>
    Co-authored-by: Lyuda Kim <lyudarkim@users.noreply.github.com>

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './CardForm.css';
 
+const INITIAL_FORM_DATA = {
+    message: ''
+}
 
-const CardForm = () => {
+const CardForm = (props) => {
+    const [cardFormData, setCardFormData] = useState(INITIAL_FORM_DATA);
+
+    const anInputChanged = (event) => {
+        const newCardFormData = {
+            ...cardFormData,
+            [event.target.name]: event.target.value
+        };
+
+        setCardFormData(newCardFormData);
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        props.createCard(cardFormData);
+
+        setCardFormData(INITIAL_FORM_DATA);
+    };
+
     return (
         <section>
-            This is the Card Form
+            <h2>New Card</h2>
+            <form onSubmit={onFormSubmit}>
+                <label htmlFor="cardMessage">Message: </label>
+                <input
+                id="cardMessage"
+                name="message"
+                type="text"
+                value={cardFormData.message}
+                onChange={anInputChanged}
+                ></input>
+                <input type="submit" value="add a card!"></input> 
+            </form>
+
         </section>
     );
 
+};
+
+CardForm.propTypes = {
+    createCard: PropTypes.func
 };
 
 export default CardForm;
