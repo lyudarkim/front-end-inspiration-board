370f119045dc4f9c7c6149ad0f010da9ebc42c17
selene castillo <secast@live.com>
2023-07-17 12:02:28 -0700

    Add anInputChanged, onFormSubmit and state to BoardForm

--- src/components/BoardForm.js
+++ src/components/BoardForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './BoardForm.css';
 
@@ -8,21 +8,41 @@ const INITIAL_FORM_DATA = {
     owner: ''
 }
 
-const BoardForm = () => {
+const BoardForm = (props) => {
+
+    const [boardFormData, setBoardFormData] = useState(INITIAL_FORM_DATA);
+
+    const anInputChanged = (event) => {
+        const newBoardFormData = {
+            ...boardFormData,
+            [event.target.name]: event.target.value
+        };
+        setBoardFormData(newBoardFormData);
+    }
+    
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        
+        props.createBoard(boardFormData);
+
+        setBoardFormData(INITIAL_FORM_DATA);
+    };
 
     return (
         <section>
             <h2>Create a New Board</h2>
-            <form>
+            <form onSubmit={onFormSubmit}>
                 <label htmlFor="boardTitle">Title:</label>
                 <input 
                 id="boardTitle"
                 name="title"
+                onChange={anInputChanged}
                 ></input>
                 <label htmlFor="boardOwner">Owner's Name:</label>
                 <input 
                 id="boardOwner"
-                name="owners name"
+                name="owner"
+                onChange={anInputChanged}
                 ></input> 
                 <input 
                 type="submit"
